322707e72590674d6fb8f05f5000f8b9dcc8fd52
Stefan Mesken <kappadistributive@gmail.com>
2024-05-16 20:56:42 +0200

    format

--- tests/LexerTests.cpp
+++ tests/LexerTests.cpp
@@ -9,21 +9,20 @@ using namespace token;
 TEST(Lexer, NextToken) {
   std::string input{"=+(){},;"};
 
-  std::vector<Token> expected{{
-      Token(TokenType::ASSIGN, "="), Token(TokenType::PLUS, "+"),
-      Token(TokenType::LPAREN, "("), Token(TokenType::RPAREN, ")"),
-      Token(TokenType::LBRACE, "{"), Token(TokenType::RBRACE, "}"),
-      Token(TokenType::COMMA, ","),
-      Token(TokenType::SEMICOLON, ";"),
-      Token()
-  }};
+  std::vector<Token> expected{
+      {Token(TokenType::ASSIGN, "="), Token(TokenType::PLUS, "+"),
+       Token(TokenType::LPAREN, "("), Token(TokenType::RPAREN, ")"),
+       Token(TokenType::LBRACE, "{"), Token(TokenType::RBRACE, "}"),
+       Token(TokenType::COMMA, ","), Token(TokenType::SEMICOLON, ";"),
+       Token()}};
 
-  Lexer l(input);
+  Lexer lexer(input);
   EXPECT_EQ(expected.size(), input.size() + 1);
   for (size_t index{0}; index < expected.size(); ++index) {
-    Token token = l.next_token();
+    Token token = lexer.next_token();
     EXPECT_EQ(expected[index], token);
   }
-  Token token = l.next_token();
+  Token token = lexer.next_token();
 }
+
 } // namespace lexer
